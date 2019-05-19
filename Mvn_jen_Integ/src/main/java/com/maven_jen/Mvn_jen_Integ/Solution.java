542ce5147e4db584e4d95b6426204c6e0a88e0f3
ram <nramchandh1994@gmail.com>
2019-05-19 15:48:17 +0530

    maven integration sample project

---
+++ Mvn_jen_Integ/src/main/java/com/maven_jen/Mvn_jen_Integ/Solution.java
@@ -0,0 +1,23 @@
+package com.maven_jen.Mvn_jen_Integ;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Solution {
+	
+	static WebDriver driver;
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:/Maven_Jen_Int/Mvn_jen_Integ/driver/chromedriver.exe");
+		
+		driver= new ChromeDriver();
+		
+		driver.get("https://www.facebook.com");
+		
+		System.out.println(driver.getTitle());
+		
+	}
+	
+
+}
